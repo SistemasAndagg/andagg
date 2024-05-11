3efa2d5998ee1e30f4642903763931a28d0816ce
SistemasAndagg <sistemas@andagg.com>
2024-05-11 12:49:00 -0600

    boton whats

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import whatsappLogo from './Public/whatsapp-logo.jpg'; 
 import './index.css';
 
 const App: React.FC = () => {
+
+  const sendWhatsAppMessage = () => {
+    // Reemplaza el mensaje con el que deseas iniciar la conversación
+    const message = encodeURIComponent("¡Hola! Estoy interesado en obtener más información sobre sus servicios.");
+    window.open(`https://wa.me/9994393319?text=${message}`, '_blank');
+  };
+
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col">
+    <div className="bg-gray-100 min-h-screen flex flex-col relative">
       {/* Header */}
       <header className="bg-blue-900 text-white py-4">
         <div className="container mx-auto flex justify-between items-center">
@@ -19,7 +27,7 @@ const App: React.FC = () => {
       </header>
 
       {/* Main content */}
-      <main className="flex-1 mt-8 mb-8"> {/* Agrega mt-8 y mb-8 para márgenes arriba y abajo */}
+      <main className="flex-1 mt-8 mb-8 overflow-auto"> {/* overflow-auto para permitir el scroll */}
         <div className="container mx-auto py-8">
           <section id="intro" className="mb-8 animate-fade-in-down">
             <h2 className="text-4xl font-bold text-gray-900 mb-4">Bienvenido a AndaGG</h2>
@@ -29,36 +37,29 @@ const App: React.FC = () => {
           <section id="services" className="mb-8">
             <h2 className="text-3xl font-bold text-gray-900 mb-4">Nuestros Servicios</h2>
             <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-              <li className="bg-white shadow-md rounded-lg p-6 transform hover:scale-105 transition-transform duration-300 animate-fade-in-left">
-                <h3 className="text-xl font-semibold mb-2">Desarrollo de software médico</h3>
-                <p>Desarrollamos software personalizado para clínicas y médicos.</p>
-              </li>
-              <li className="bg-white shadow-md rounded-lg p-6 transform hover:scale-105 transition-transform duration-300 animate-fade-in-right">
-                <h3 className="text-xl font-semibold mb-2">Asesoramiento contable y administrativo</h3>
-                <p>Ofrecemos servicios de contabilidad y administración adaptados a tu negocio.</p>
-              </li>
-              <li className="bg-white shadow-md rounded-lg p-6 transform hover:scale-105 transition-transform duration-300 animate-fade-in-left">
-                <h3 className="text-xl font-semibold mb-2">Creación de contenido médico</h3>
-                <p>Generamos contenido médico relevante y de calidad para tu audiencia.</p>
-              </li>
-              <li className="bg-white shadow-md rounded-lg p-6 transform hover:scale-105 transition-transform duration-300 animate-fade-in-right">
-                <h3 className="text-xl font-semibold mb-2">Estrategias de marketing</h3>
-                <p>Diseñamos y ejecutamos estrategias de marketing efectivas para profesionales de la salud.</p>
-              </li>
+              {/* Servicios */}
             </ul>
           </section>
 
-          <section id="contact" className="animate-fade-in-up">
+          <section id="contact" className="animate-fade-in-up mb-8">
             <h2 className="text-3xl font-bold text-gray-900 mb-4">Contacto</h2>
             <p className="text-lg text-gray-700 mb-2">Ponte en contacto con nosotros para más detalles.</p>
             <p className="text-lg text-gray-700 mb-2">Email: info@andagg.com</p>
             <p className="text-lg text-gray-700">Teléfono: +123456789</p>
           </section>
+
+          {/* Agregar más contenido aquí */}
+
         </div>
+
+        {/* Floating WhatsApp Button */}
+        <button onClick={sendWhatsAppMessage} className="fixed bottom-8 right-8 md:bottom-16 md:right-16 lg:right-20 bg-green-500 hover:bg-green-600 text-white font-bold py-4 px-4 rounded-full shadow-lg z-10">
+          <img src={whatsappLogo} alt="WhatsApp" className="w-6 h-6" />
+        </button>
       </main>
 
       {/* Footer */}
-      <footer className="bg-gray-900 text-white py-4 mt-8"> {/* Agrega mt-8 para margen arriba */}
+      <footer className="bg-gray-900 text-white py-4 mt-8">
         <div className="container mx-auto text-center">
           <p className="text-sm animate-bounce">&copy; {new Date().getFullYear()} AndaGG. Todos los derechos reservados.</p>
         </div>
