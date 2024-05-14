60583202dcbe5c6ccc28e71e6eb84c11d0adbd63
SistemasAndagg <sistemas@andagg.com>
2024-05-14 11:35:12 -0600

    fondo y ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+// Importaciones de módulos y componentes
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import { faCashRegister } from '@fortawesome/free-solid-svg-icons';
 import { faChartLine, faFileInvoiceDollar, faShoppingCart, faCalculator, faNetworkWired, faBullhorn, faPenSquare } from '@fortawesome/free-solid-svg-icons';
 import './index.css';
-import AndaggLogo from './public/Andagg_Blanco.png'; // Ruta de la imagen del logo
+import Fondo from './public/gente-negocios-dandose-mano.jpg'; // Ruta de la imagen del logo
 
+
+// Definición de la interfaz para las tarjetas de servicio
 interface ServiceCardProps {
   icon: JSX.Element;
   title: string;
   description: string;
 }
 
+// Componente para las tarjetas de servicio
 const ServiceCard: React.FC<ServiceCardProps> = ({ icon, title, description }) => (
   <div className="bg-white rounded-lg shadow-lg p-6 flex flex-col items-center cursor-pointer transition-transform transform-gpu hover:-translate-y-2 hover:shadow-xl">
     <div className="text-3xl text-gray-800 mb-4">{icon}</div>
@@ -20,22 +24,21 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ icon, title, description }) =
   </div>
 );
 
+// Componente principal de la aplicación
 const App: React.FC = () => {
-
+  // Función para enviar mensajes por WhatsApp
   const sendWhatsAppMessage = () => {
-    // Reemplaza el mensaje con el que deseas iniciar la conversación
     const message = encodeURIComponent("¡Hola! Estoy interesado en obtener más información sobre sus servicios.");
     window.open(`https://wa.me/9994393319?text=${message}`, '_blank');
   };
 
+  // Renderización del componente
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col relative">
-      {/* Header */}
+<div className="min-h-screen flex flex-col relative">
+      {/* Sección del encabezado */}
       <header className="bg-blue-900 text-white py-4 px-8">
         <div className="container mx-auto flex justify-between items-center">
-          <div className='className=" w-52 h-24"'>
-          <img src={AndaggLogo} alt="AndaGG" className=" w-24 h-24" /> 
-          </div>
+          
           <h1 className="text-3xl font-bold animate-pulse">AndaGG</h1>
           <nav>
             <ul className="flex space-x-4">
@@ -47,17 +50,20 @@ const App: React.FC = () => {
         </div>
       </header>
 
-      {/* Main content */}
-      <main className="flex-1 mt-8 mb-8 overflow-auto"> {/* overflow-auto para permitir el scroll */}
+      {/* Sección del cuerpo */}
+      <main className=" overflow-auto" style={{backgroundImage: `url(${Fondo})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
         <div className="container mx-auto py-8">
-          <section id="intro" className="mb-8 animate-fade-in-down">
-            <h2 className="text-4xl font-bold text-gray-900 mb-4">Bienvenido a AndaGG</h2>
-            <p className="text-lg text-gray-700">Brindamos soluciones integrales para clínicas y médicos, desde software especializado hasta estrategias de marketing.</p>
+          <section id="intro" className="mb-8 animate-fade-in-down bg-white rounded-lg shadow-lg flex flex-col items-center py-4">
+            <h2 className="text-4xl font-bold text-gray-900 mb-4 ">Bienvenido a AndaGG</h2>
+            <p className="text-lg text-gray-700 ">Brindamos soluciones integrales para clínicas y médicos, desde software especializado hasta estrategias de marketing.</p>
+            <h2 className="text-3xl font-bold text-gray-900 mb-4 py-4">Nuestros Servicios:</h2>
           </section>
 
+          {/* Sección de servicios */}
           <section id="services" className="mb-8">
-            <h2 className="text-3xl font-bold text-gray-900 mb-4">Nuestros Servicios</h2>
+      
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+              {/* Renderización de las tarjetas de servicio */}
               <ServiceCard
                 icon={<FontAwesomeIcon icon={faChartLine} />}
                 title="Gestión Administrativa"
@@ -101,7 +107,8 @@ const App: React.FC = () => {
             </div>
           </section>
 
-          <section id="contact" className="animate-fade-in-up mb-8">
+          {/* Sección de contacto */}
+          <section id="contact" className="animate-fade-in-up mb-8 bg-white rounded-lg shadow-lg px-4 py-2">
             <h2 className="text-3xl font-bold text-gray-900 mb-4">Contacto</h2>
             <p className="text-lg text-gray-700 mb-2">Ponte en contacto con nosotros para más detalles.</p>
             <p className="text-lg text-gray-700 mb-2">Email: info@andagg.com</p>
@@ -112,14 +119,14 @@ const App: React.FC = () => {
 
         </div>
 
-        {/* Floating WhatsApp Button */}
+        {/* Botón flotante de WhatsApp */}
         <button onClick={sendWhatsAppMessage} className="fixed bottom-8 right-8 md:bottom-16 md:right-16 lg:right-2 bg-green-500 hover:bg-green-600 text-white font-bold py-4 px-4 rounded-full shadow-lg z-10">
           <FontAwesomeIcon icon={faWhatsapp} className="w-6 h-6" />
         </button>
       </main>
 
-      {/* Footer */}
-      <footer className="bg-gray-900 text-white py-4 mt-8">
+      {/* Sección del pie de página */}
+      <footer className="bg-gray-900 text-white py-4">
         <div className="container mx-auto text-center">
           <p className="text-sm">&copy; {new Date().getFullYear()} AndaGG. Todos los derechos reservados.</p>
         </div>
